abf72b4a3bbecd2bb86155ee24715c9eb88f3472
Haihua Xiao <hhxiao@gmail.com>
2014-12-19 14:08:09 +0800

    add dependency to spring-security-core

---
+++ grails-app/conf/BuildConfig.groovy
@@ -0,0 +1,24 @@
+grails.project.work.dir = 'target'
+
+grails.project.repos.default = "grailsCentral"
+
+grails.project.dependency.resolver = "maven"
+grails.project.dependency.resolution = {
+
+    inherits 'global'
+    log 'warn'
+
+    repositories {
+        grailsCentral()
+        mavenLocal()
+        mavenCentral()
+    }
+
+    plugins {
+        compile ":spring-security-core:1.2.7.3"
+
+        build ':release:3.0.1', ':rest-client-builder:1.0.3', {
+            export = false
+        }
+    }
+}
