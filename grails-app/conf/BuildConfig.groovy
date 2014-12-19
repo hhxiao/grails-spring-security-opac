8eeb9a2da5ae9e364f0f51ee26737f1bf6e3459c
Haihua Xiao <hhxiao@gmail.com>
2014-12-19 14:17:03 +0800

    Downgrade release plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -17,7 +17,7 @@ grails.project.dependency.resolution = {
     plugins {
         compile ":spring-security-core:1.2.7.3"
 
-        build ':release:3.0.1', ':rest-client-builder:1.0.3', {
+        build ':release:2.2.1', ':rest-client-builder:1.0.3', {
             export = false
         }
     }
