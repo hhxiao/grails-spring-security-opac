f1164522b701a738b59edc7c8109b62bd08092f9
haihxiao <haihxiao@cisco.com>
2015-11-24 20:19:43 +0800

    use or-auth_valid to login

--- src/groovy/org/grails/plugin/springsecurity/opac/OpacAuthenticator.groovy
+++ src/groovy/org/grails/plugin/springsecurity/opac/OpacAuthenticator.groovy
@@ -1,20 +1,18 @@
 package org.grails.plugin.springsecurity.opac
-
 import groovy.util.slurpersupport.GPathResult
 import org.apache.commons.logging.Log
 import org.apache.commons.logging.LogFactory
-
 /**
  * Created by haihxiao on 2014/12/18.
  */
 class OpacAuthenticator {
     private static final Log logger = LogFactory.getLog(OpacAuthenticator.class)
 
-    String url = 'http://aleph-01.clcn.net.cn:8991'
-    String library = 'stl50'
+    String url
+    String library
 
     GPathResult authenticate(username, password) {
-        String url = "${url}/X?op=bor-auth&bor-id=${username}&verification=${password}&library=${library}"
+        String url = "${url}/X?op=bor-auth_valid&id=${username}&verification=${password}&library=${library}"
         String result = new URL(url).getText('UTF-8')
         def xmlSlurper = new XmlSlurper()
         def xml = xmlSlurper.parseText(result)
